8cf4e3fcad907247afe844fd43a46532264fe9a1
Stein Inge Morisbak <stein.inge.morisbak@hkraft.no>
2019-01-15 17:27:20 +0100

    cleanup of hentYr function

--- functions/index.js
+++ functions/index.js
@@ -6,46 +6,41 @@ var parseString = require('xml2js').parseString;
 admin.initializeApp(functions.config().firebase);
 
 exports.hentYr = functions.https.onRequest((req, res) => {
-  console.log("Hent Værdata");
+  console.log("Hent værdata");
   request('https://www.yr.no/sted/Norge/Rogaland/Haugesund/Eivindsvatnet/varsel.xml', (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    //console.log('body:', body); // Print the JSON for the YR varsel.
+    console.log('the error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code
+    console.log('body:', body); // Print the JSON for the YR varsel.
     parseString(body, (err, result) => {
       var weatherRef = admin.firestore().collection('weather');
-      //console.dir(result);
+      console.dir(result);
       if (err !== null)
-        console.log(err);
+        console.log('not null error: ', err);
 
-      var weather = new Object();
       var dateTemp = result.weatherdata.meta[0].lastupdate[0] + "+02:00";
       console.log("dateTemp", dateTemp);
       console.log("dateTemp", Date.parse(dateTemp));
-      weather.lastupdate = new Date(dateTemp);
+
+      var weather = new Object();
+
+      weather.lastupdate = admin.firestore.Timestamp.fromDate(new Date(dateTemp));
       weather.link = result.weatherdata.links[0].link[2].$.url;
       weather.symbol = result.weatherdata.forecast[0].tabular[0].time[0].symbol[0];
       weather.wind = result.weatherdata.forecast[0].tabular[0].time[0].windSpeed[0].$;
-      weather.time = getDateNoTime(result.weatherdata.forecast[0].tabular[0].time[0].$);
+      weather.time = admin.firestore.Timestamp.fromDate(getDateNoTime(result.weatherdata.forecast[0].tabular[0].time[0].$))
       weather.temperature = result.weatherdata.forecast[0].tabular[0].time[0].temperature[0].$.value;
+
       var tempForecast = result.weatherdata.forecast[0].tabular[0].time;
       console.log("length", tempForecast.length);
-      for (var i = 0; i < tempForecast.length; i++) {
 
+      for (var i = 0; i < tempForecast.length; i++) {
         console.log("periode", tempForecast[i].$.period);
         if (tempForecast[i].$.period === "3") {
           weather.forecast = new Object();
-          console.log("YEAH");
           weather.forecast.symbol = tempForecast[i].symbol[0];
           weather.forecast.wind = tempForecast[i].windSpeed[0].$;
-          weather.forecast.time = getDateNoTime(tempForecast[i].$);
+          weather.forecast.time = admin.firestore.Timestamp.fromDate(getDateNoTime(tempForecast[i].$));
           weather.forecast.temperature = tempForecast[i].temperature[0].$.value;
-
-          if ((new Date(weather.time.from)).setHours(0, 0, 0, 0) === (new Date(weather.forecast.time.from)).setHours(0, 0, 0, 0)) {
-            console.log("samme dag");
-            weather.forecast.sameDay = true;
-          }
-          else {
-            weather.forecast.sameDay = false;
-          }
           break;
         }
       }
@@ -54,7 +49,6 @@ exports.hentYr = functions.https.onRequest((req, res) => {
 
       console.log("ISOString" + weather.lastupdate.toISOString());
       weatherRef.doc(weather.lastupdate.toISOString()).set(weather);
-      //result.lastupdate = result.weatherdata.meta[0].lastupdate[0];
       return res.send(weather);
 
     });
