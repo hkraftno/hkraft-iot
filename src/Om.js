cf2e3db02affc0b711871011c3b6f6e3cca23b66
Stein Inge Morisbak <stein.inge.morisbak@hkraft.no>
2019-01-17 18:52:57 +0100

    Edited the Om page

--- src/Om.js
+++ src/Om.js
@@ -53,7 +53,11 @@ class Om extends Component {
                 <p />
                 Det er ikke bare hjemmene våre som blir smartere med ny teknologi – det ligger et samfunnsmessig mye større potensiale andre steder. For eksempel i kommuneforvaltning og i det å bygge byer, regioner og kommuner til smartere og mer effektive samfunn.
                 <p />
-                På Alvanuten i Aksdal har Haugaland Kraft montert en sender som over lange avstander kan kommunisere med sensorer i mange varianter. Sensorene kan måle luftkvalitet, temperatur, bevegelse, helling, posisjon, retning, trykk, vind, sol og mye mer. De kan plasseres i vann, sjø, luft, jord, på eller inne i gjenstander. Et stort mulighetsrom som kun begrenses av kreativitet. Eksempler på anvendelser er måling av badetemperaturer, telling av turgåere, syklister, ledige parkeringsplasser, fyllingsgrad i søppelkasser, kumlokk som er i ferd med å tettes, rør som har sprekker eller er tette, brannfeller, rottefeller, mørke gatelys, gryende oversvømmelser, isete veier, luftkvalitet, vær, vannstand og kloakk. Stort sett alt som omgir oss kan måles, identifiseres og rapporteres. Fremover vil Haugaland Kraft lage flere nyttige tjenester som baserer seg på sensorer.
+                På Alvanuten i Aksdal har Haugaland Kraft montert en sender som over lange avstander kan kommunisere med sensorer i mange varianter. Sensorene kan måle luftkvalitet, temperatur, bevegelse, helling, posisjon, retning, trykk, vind, sol og mye mer. De kan plasseres i vann, sjø, luft, jord, på eller inne i gjenstander. Et stort mulighetsrom som kun begrenses av kreativitet.
+                <p />
+                Eksempler på anvendelser er måling av badetemperaturer, telling av turgåere, syklister, ledige parkeringsplasser, fyllingsgrad i søppelkasser, kumlokk som er i ferd med å tettes, rør som har sprekker eller er tette, brannfeller, rottefeller, mørke gatelys, gryende oversvømmelser, isete veier, luftkvalitet, vær, vannstand og kloakk.
+                <p />
+                Stort sett alt som omgir oss kan måles, identifiseres og rapporteres. Fremover vil Haugaland Kraft lage flere nyttige tjenester som baserer seg på ting som er koblet til Internett.
                 </Typography>
                 <Typography variant="title" align="left" gutterBottom>
                 Badetemperaturer
@@ -72,14 +76,14 @@ class Om extends Component {
                 to="https://www.facebook.com/HaugalandKraft/"
               target="_blank">facebook</ReactGA.OutboundLink> eller via tilbakemeldingsknappen på siden her, hvis det er noe du lurer på, er inspirert til å lage noe lignende selv eller bare vil gi en tilbakemelding
                 </Typography>
-                <Typography variant="title" align="left" gutterBottom>
+{/*                 <Typography variant="title" align="left" gutterBottom>
                 Om bruk av informasjonskapsler
                 </Typography>      
                 <Typography align="left" gutterBottom>
-                Haugaland Kraft IoT bruker informasjonskapsler fra Google Analytics, Hotjar.com og Facebook for å analysere bruken av tjenesten Bade-Anna. Disse tjenestene samler inn typisk informasjon som nettlesern du bruker, hvilke sider du har besøk, hvor du kom fra, hva slags enhet du bruker og hvor lenge du har vært på siden.<br/> <br/>
+                Haugaland Kraft IoT bruker informasjonskapsler fra Google Analytics, Hotjar.com og Facebook for å analysere bruken av tjenesten Bade-Anna. Disse tjenestene samler inn typisk informasjon som nettlesern du bruker, hvilke sider du har besøkt, hvor du kom fra, hva slags enhet du bruker og hvor lenge du har vært på siden.<br/> <br/>
                 Dette gjør vi for å kunne lage siden bedre og lære litt opp hvordan man kan få trafikk inn på siden. <br/> <br/>
                 Ønsker du at disse kapselene ikke skal benyttes kan du fjerne de under instilllinger i nettleseren din. Der kan du også skru av bruk av slike inforemasjonskapsler helt.  Mener du at jeg burde lage funksjonalitet for å gjøre dette på en enklere måte, så ta kontakt på facebook eller opp i tilbakemeldingsknappen. Det er interessant om å høre om du synes dette ikke er greit. Ellers håper jeg du er fornøyd med tjenesten og fortsetter å benytte den selv om vi måler litt om hvordan den brukes.
-                </Typography>
+                </Typography> */}
 
               <ReactGA.OutboundLink 
                 eventLabel="facebook"
