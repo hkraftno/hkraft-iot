c02bd08685950c5e65def12eaeb711b970e34104
Karl Gustav Roksund <KarlGustav@unimicro.no>
2019-01-28 10:46:39 +0100

    Cleanup of the code in functions/index.js (indentation++)

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,11 @@ firestore.settings(settings);
 const request = require('request');
 
 exports.decodeSensorData = functions.https.onRequest((req, res) => {
-
-  console.log('decodeSensorData received [', req.method + ' ]: ' + req.body);
+  console.log('decodeSensorData received [', req.method, ']:', JSON.stringify(req.body));
 
   if (req.method === 'POST') {
     var sensorData = req.body;
-    if (typeof sensorData === "object")
-      sensorData = JSON.stringify(sensorData);
+    if (typeof sensorData === "object") sensorData = JSON.stringify(sensorData);
     sensorData = JSON.parse(sensorData);
     const devEUI = sensorData.deveui;
     const doc = devEUI + '/' + sensorData.timestamp
@@ -21,8 +19,9 @@ exports.decodeSensorData = functions.https.onRequest((req, res) => {
 
     console.log('Posting for decoding: ', sensorData);
 
-    request.get(`https://us-central1-hkraft-iot.cloudfunctions.net/parse_${sensorType}/${sensorData.payload}`
-    }, (error, response, body) => {
+    request.get(
+      `https://us-central1-hkraft-iot.cloudfunctions.net/parse_${sensorType}/${sensorData.payload}`
+    , (error, response, body) => {
       if (error !== null) {
         console.log('error:', error);
         return response.sendStatus(500);
@@ -38,35 +37,34 @@ exports.decodeSensorData = functions.https.onRequest((req, res) => {
       };
       console.log('Legger inn:', item);
       firestoreRef.set(item).then(snapshot => { return console.log(item, "lagt inn i " + doc); })
-        .catch(error => {
-          console.log('error: ', error)
-          return response.sendStatus(500);
-        });
+      .catch(error => {
+        console.log('error: ', error)
+        return response.sendStatus(500);
+      });
       return res.send(body);
-    })
+    });
   }
   console.log('returning 405')
   return res.sendStatus(405);
 });
 
 exports.postSensorData = functions.https.onRequest((req, res) => {
-  console.log('postSensorData received [', req.method + ' ]: ' + req.body);
+  let body = JSON.stringify(req.body);
+  console.log('postSensorData received [', req.method, ']:', body);
   if (req.method === 'POST') {
-    var request = req.body;
-    if (typeof request === "object")
-      request = JSON.stringify(request);
-    request = request.replace(/([{,])(\s*)([A-Za-z0-9_-]+?)\s*:/g, '$1"$3":')
-    request = request.replace(/'/g, '"');
-    request = request.replace(/\[Object\]/g, '"[Object]"');
-    console.log('postSensorData cleaned up JSON: ', request);
-    var reqObj = JSON.parse(request);
+    if (typeof body === "object") body = JSON.stringify(body);
+    body = body.replace(/([{,])(\s*)([A-Za-z0-9_-]+?)\s*:/g, '$1"$3":')
+    body = body.replace(/'/g, '"');
+    body = body.replace(/\[Object\]/g, '"[Object]"');
+    console.log('postSensorData cleaned up JSON: ', body);
+    var reqObj = JSON.parse(body);
     console.log('postSensorData object: ', reqObj);
     const firestoreRef = firestore.doc(`from_thingpark/${new Date(reqObj.DevEUI_uplink.Time).valueOf()}`);
     firestoreRef.set(reqObj).then(snapshot => { return console.log(reqObj, "lagt inn i from_thingpark"); })
-      .catch(error => {
-        console.log('error: ', error)
-        return res.sendStatus(500);
-      });
+    .catch(error => {
+      console.log('error: ', error)
+      return res.sendStatus(500);
+    });
     console.log('returning 201')
     return res.sendStatus(201);
   } else {
@@ -88,8 +86,7 @@ exports.sensorDataUpdate = functions.firestore
         deveui: sensorData.DevEUI_uplink.DevEUI
       }
     }, (error, response, body) => {
-      if (error !== null)
-        console.log('the error:', error);
+      if (error !== null) console.log('the error:', error);
       return 0;
     })
   });
