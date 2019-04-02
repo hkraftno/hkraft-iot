dbd32af2ab61010abec15f4597eec9ba091daad5
Karl Gustav Roksund <kkgg84+git@gmail.com>
2019-04-02 11:05:20 +0200

    Moved the parsers into objects with arrays of DevEUIs
    Because that scales better, simpler to have one parser object with an
    array of n DevEUIs than to have one object per sensor.

--- functions/index.js
+++ functions/index.js
@@ -14,28 +14,20 @@ exports.postSensorData = functions.https.onRequest((req, res) => {
     const coordinates = {lat: Number(uplink.CustomerData.loc.lat), lng: Number(uplink.CustomerData.loc.lon)}
     const fromThingparkRef = firestore.doc(`from_thingpark/${ts}-${uplink.DevEUI}`);
     const measurementRef = firestore.doc(`sensors/lora/${uplink.DevEUI}/${ts}`);
-    const sensorParsersRef = firestore.doc('sensors/lora');
 
     return fromThingparkRef
     .set(req.body)
     .then(() => console.log('Stored data from thinkpark: OK'))
-    .then(() => sensorParsersRef.get())
-    .then(doc => doc.data())
-    .then(sensors => {
-      if (!sensors[uplink.DevEUI]){
-        throw new Error(`There doesn't exist a parser for DevEUI ${uplink.DevEUI}`);
-      }
-      return sensors;
-    })
-    .then(sensors => rp.get(`${sensors[uplink.DevEUI]}/${uplink.payload_hex}`))
-    .then(response => JSON.parse(response))
+    .then(() => getSensorParser(uplink.DevEUI))
+    .then(parser => rp.get(`${parser.parserURL}/${uplink.payload_hex}`))
+    .then(JSON.parse)
     .then(parsed => Object.assign(parsed, {timestamp: ts, coordinates: coordinates}))
     .then(parsed => measurementRef.set(parsed))
     .then(() => console.log('Stored parsed payload: OK'))
     .then(() => res.sendStatus(201))
     .catch(error => {
-      console.log('Error:', error);
-      return res.status(500).send(error);
+      console.error('Error:', error.message);
+      return res.status(500).send(error.message);
     })
   } else {
     return res.sendStatus(405);
@@ -44,9 +36,22 @@ exports.postSensorData = functions.https.onRequest((req, res) => {
 
 exports.updateLatest = functions.firestore
 .document(`sensors/lora/{sensorId}/{time}`)
-.onCreate((snap, context) =>
-  firestore
-  .doc(`latest/${context.params.sensorId}`)
-  .set(snap.data())
-  .then(() => console.log(`Updated 'latest/${context.params.sensorId}' to point to newest measurement`))
+.onCreate((snap, context) => firestore
+.doc(`latest/${context.params.sensorId}`)
+.set(snap.data())
+.then(() => console.log(`Updated 'latest/${context.params.sensorId}' to point to newest measurement`))
 );
+
+function getSensorParser(DevEUI) {
+  return firestore.collection('sensors/lora/parsers')
+  .where('DevEUIs', 'array-contains', DevEUI)
+  .get()
+  .then(query => {
+    if (query.docs.length === 0){
+      throw new Error(`There doesn't exist a parser for DevEUI ${DevEUI}`);
+    } else if (query.docs.length > 1) {
+      throw new Error(`There are more than one parser that matches the DevEUI ${DevEUI}: ${query.docs.map(s => s.id)}`);
+    }
+    return query.docs[0].data();
+  });
+}
