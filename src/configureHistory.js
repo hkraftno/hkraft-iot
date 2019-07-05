0480775d31f03d498531fbe6dc23c3148d9669d3
Stein Inge Morisbak <stein.inge.morisbak@hkraft.no>
2019-07-05 10:16:57 +0200

    removed react app

--- src/configureHistory.js
+++
@@ -1,9 +0,0 @@
-// configureHistory.js
-import { createBrowserHistory, createHashHistory } from 'history'
-
-export default function configureHistory() {
-    var history = window.matchMedia('(display-mode: standalone)').matches ? "Hash": "Browser";
-  return window.matchMedia('(display-mode: standalone)').matches
-    ? createHashHistory()
-    : createBrowserHistory()
-}
